cd1b50f319f3843904965e682602dec3282709fa
Jesús Hernández <jesus.hernandezmoreno@outlook.com>
2023-07-17 12:26:01 -0600

    Change assets

--- src/components/canvas/Shirt.jsx
+++ src/components/canvas/Shirt.jsx
@@ -3,12 +3,12 @@ import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
-
+import ShirtModel from '../../assets/shirt_baked.glb?url';
 import state from '../../store';
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+  const { nodes, materials } = useGLTF(ShirtModel);
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
